25c3229e653489bbb84bdb60584491809e342bc7
sdk-banuba <71497187+sdk-banuba@users.noreply.github.com>
2023-03-27 13:41:51 +0300

    Update BanubaClientToken.kt

--- client_token/com/banuba/sdk/example/common/BanubaClientToken.kt
+++ client_token/com/banuba/sdk/example/common/BanubaClientToken.kt
@@ -4,4 +4,4 @@ package com.banuba.sdk.example.common
  * Client token for Banuba SDK. Consider obfuscation in release app.
  */
 
-internal const val BANUBA_CLIENT_TOKEN: String = "Qk5CIEv3NqPBD1XD9ECF6usFXQmeKdu+qODvk9GtVZ+nB5FCwCaaEOGvZdpJ3AnyBc7+xgycq114RCMVHgrx+cARLJqNjTBzIkqcuTzdn8n9709QF+Ef4QMu2GP6eb5XboVsle+eSmZeLGKv7k3YYXuky9fxjzQFfdCXMAx7O5xbwk9itnSBfxoNJzxEF5doHFrQ5F1SAj+D3cZyEQgPsDYBuGF52Im9/QRR5EpjHj89Urt/yQ0xIWWZMvRDr39szCXoDTAfY3EsGQUt/suj4jiT9Qr6zXC/Cw=="
+internal const val BANUBA_CLIENT_TOKEN: String = "Qk5CID29wshzurCiygXU6YmnTvgoppivBlMlLZhHXAT4NzoDagJgMtTsrLb3uu+vI0Lcus6JwxAjfexmo5SMBCx3OVcHbHjgAjJy5HLogIe/NjhS3/892IbGqKTc4gt4KP7PbWSwL1H3j49sBdsc8Lu8XP+XuNuX47foVLdV/is3qzQVFqSnRFoUcDXKPYNw0BodxBupZ1fKk7aAlgX3hJAh4Y4ZO78FBl3qm9Hs4tSQLlQ4IRtjJICOfSuvyrMtEPwIoBfIRCDjGY3dhyp8pyAcZVusLvBpvXY="
